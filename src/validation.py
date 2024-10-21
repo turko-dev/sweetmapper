c86a0848ef9525dc76969fc236cbe83bdad60b63
turko-dev <turkoai@protonmail.com>
2024-10-21 16:50:02 +0100

    single file app & added test csv

--- src/validation.py
+++
@@ -1,7 +0,0 @@
-def StringValidation(string):
-    if(isinstance(string, str)): return True
-    else: return False
-
-def BooleanValidation(boolean):
-    if(isinstance(boolean, bool)): return True
-    else: return False
