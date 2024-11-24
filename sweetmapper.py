1b9f4814972c7235bedf5edf563c8ca64425e924
turko-dev <turkoai@protonmail.com>
2024-11-24 16:39:22 +0000

    revamped sweetmapper 2.0

--- sweetmapper.py
+++ sweetmapper.py
@@ -1,4 +1,3 @@
-
 import os
 from colorama import Fore
 import googlemaps
@@ -6,276 +5,114 @@ import pandas as pd
 import os
 import time
 
-def StringValidation(string):
-    if(isinstance(string, str)): return True
-    else: return False
+#Add Cities in here
+cities = {
+    "manchester": [53.4808, -2.2426]
+    
+}
+placeType = "Coffee Shops"
+radius = 20
 
-def BooleanValidation(boolean):
-    if(isinstance(boolean, bool)): return True
-    else: return False
+language = "en-UK"
+filename = "lol"
+def areaSearch(mode, key, placeType, location, radius, language, filename):
+    
+    if(isinstance(location, dict)):
+        pass
+    elif(isinstance(location, str)):
+        city = location
+        location = {"lat": cities[city][0], "lng": cities[city][1]}
 
-class Places:
-    def __init__(self, key, placeType, location, radius, region, language="en-UK", open_now=False):
-        #api key validation
-        if(StringValidation(key)): self.key = key
-        else: raise ValueError("key needs to be of type str")
-
-        #placeType validation
-        if(StringValidation(placeType)): self.placeType = placeType
-        else: raise ValueError("placeType needs to be of type str")
-
-        #location validation
-        if(StringValidation(location)): self.location = location
-        else: raise ValueError("location needs to be of type str")
-        
-        #radius validation
-        if(StringValidation(radius)): self.radius = radius
-        else: raise ValueError("radius needs to be of type str")
-
-        #region validation
-        if(StringValidation(region)): self.region = region
-        else: raise ValueError("region needs to be of type str")
-
-        #language validation
-        if(StringValidation(language)): self.language = language
-        else: raise ValueError("language needs to be of type str")
-        
-        #open_now validation
-        if(BooleanValidation(open_now)): self.open_now = open_now
-        else: raise ValueError("open_now needs to be of type bool")
-
-        #Main Places DataFrame
-        self.dataFrame = None
-    """Getters & Setters for all parameters"""
-    #PlaceType==============================================
-    def getPlaceType(self): return self.placeType
-    def setPlaceType(self, newPlaceType):
-        if(StringValidation(newPlaceType)): self.placeType = newPlaceType
-        else: raise ValueError("newPlaceType needs to be of type str")
-
-    #Location==============================================
-    def getLocation(self): return self.location
-    def setLocation(self, newLocation):
-        if(StringValidation(newLocation)): self.location = newLocation
-        else: raise ValueError("newLocation needs to be of type str")
-
-    #Radius==============================================
-    def getRadius(self): return self.radius
-    def setRadius(self, newRadius):
-        if(StringValidation(newRadius)): self.radius = newRadius
-        else: raise ValueError("newRadius needs to be of type str")
-
-    #Region==============================================
-    def getRegion(self): return self.region
-    def setRegion(self, newRegion):
-        if(StringValidation(newRegion)): self.region = newRegion
-        else: raise ValueError("newRegion needs to be of type str")
-
-    #Language==============================================
-    def getLanguage(self): return self.language
-    def setLanguage(self, newLanguage):
-        if(StringValidation(newLanguage)): self.language = newLanguage
-        else: raise ValueError("newLanguage needs to be of type str")
-
-    #OpenNow==============================================
-    def getOpenNow(self): return self.open_now
-    def setOpenNow(self, newOpenNow):
-        if(BooleanValidation(newOpenNow)): self.open_now = newOpenNow
-        else: raise ValueError("newOpenNow needs to be of type bool")
-
-    """Configuration Commands"""
-
-    #Version
-    #def version(self): return f"{self.config["version"]}"
-
-    #__str__ function
-    #def __str__(self): return f"<{self.config["name"]}/{self.config["version"]}.object.Places>"
-
-
-    """Functionality"""
-    #Search any region of the world and return a table of information
-    def areaSearch(self):
-        
-        """
-        Returns a pandas DataFrame with a maximum of 60 results
-        """
-        print("Give Sweetmapper up to 60 seconds to retrieve data from Google's API")
-
-     
-        client = googlemaps.Client(key=self.key)
-
-        df = pd.DataFrame({
-            "name": [],
-            "place_id": [],
-            "address": []
-        })
-
-        time.sleep(2)
-
-        pageSearch = client.places(self.placeType, 
-                                    location=self.location, 
-                                    radius=self.radius,
-                                    region=self.region,
-                                    language=self.language,
-                                    open_now=self.open_now,
+    client = googlemaps.Client(key=key)
+    time.sleep(3)
+    count = 0
+    placeIDList = []
+    nameList = []
+    numberList = []
+    addressList = []
+    websiteList = []
+    pageSearch = client.places(placeType, 
+                                    location=location,
+                                    language=language,
+                                    radius=radius,
+                                    open_now=False,
                                     )
-        
-        df1 = pd.DataFrame({
-            "name": [x["name"] for x in pageSearch["results"]],
-            "place_id": [x["place_id"] for x in pageSearch["results"]],
-            "address": [x["formatted_address"] for x in pageSearch["results"]]
-        })
+    for x in pageSearch["results"]:
+        count = count + 1
+        placeIDList.append(x["place_id"])
+    time.sleep(3)
+    skipNext = False
+    if(skipNext == False):
+        if("next_page_token" in pageSearch):
+            pageSearch2 = client.places(placeType, 
+                                        location=location,
+                                        language=language,
+                                        radius=radius,
+                                        open_now=False,
+                                        page_token=pageSearch["next_page_token"]
+                                        )
+            for y in pageSearch2["results"]:
+                count = count + 1
+                placeIDList.append(y["place_id"])
+    else:
+        skipNext = True
+    time.sleep(3)
+    if(skipNext == False):
+        if("next_page_token" in pageSearch2):
+            pageSearch3 = client.places(placeType, 
+                                        location=location,
+                                        language=language,
+                                        radius=radius,
+                                        open_now=False,
+                                        page_token=pageSearch2["next_page_token"]
+                                        )
+            for z in pageSearch3["results"]:
+                count = count + 1
+                placeIDList.append(z["place_id"])
+    time.sleep(3)
+    
 
-        df = pd.concat([df, df1], ignore_index=True)
+    if(mode == "print" or mode == "both"):
 
-        def passToken(token):
-
-            df = pd.DataFrame({
-                            "name": [],
-                            "place_id": [],
-                            "address": []
-                        })
-            while True:
-
-                token = token
-
-                if token == "":
-
-                    break
-
-                else:
-
-                    time.sleep(2)
-
-                    searchAgain = client.places(self.placeType, 
-                                    location=self.location, 
-                                    radius=self.radius,
-                                    region=self.region,
-                                    language=self.language,
-                                    open_now=self.open_now,
-                                    page_token=token
-                                    )
-                    
-                    dfName = [x["name"] for x in searchAgain["results"]]
-
-                    dfPlace_id = [x["place_id"] for x in searchAgain["results"]]
-
-                    dfAddress = [x["formatted_address"] for x in searchAgain["results"]]
-
-                    df2 = pd.DataFrame({
-                        "name": dfName,
-                        "place_id": dfPlace_id,
-                        "address": dfAddress
-                    })
-                    
-                    df = pd.concat([df, df2])
-
-                    if "next_page_token" in searchAgain:
-
-                        token = searchAgain["next_page_token"]
-                    else:
-                        
-                        token = ""
-            return df
-        
-        dataFrameLoop = passToken(pageSearch["next_page_token"])
-
-        df = pd.concat([df, dataFrameLoop], ignore_index=True)
-
-        return df
-
-    def getPlaceIds(self, df):
-
-        return [x for x in df.get("place_id")]
-
-
-
-    def toCSV(self, listofids, fileName, verbose=False):
-
-        key = None
-
-        #with open("key.txt", "r") as apiKey: key = apiKey.readline()
-
-        client = googlemaps.Client(key=self.key)    
-
-        nameList = []
-
-        numberList = []
-
-        addressList = []
-
-        websiteList = []
-
-        for x in listofids:
-
-            placeSearch = client.place(x)
+        for a in placeIDList:
+            placeIDSearch = client.place(a)
+            if "name" in placeIDSearch["result"]: print(placeIDSearch["result"]["name"])
+            if "formatted_phone_number" in placeIDSearch["result"]: print(placeIDSearch["result"]["formatted_phone_number"])
+            if "formatted_address" in placeIDSearch["result"]: print(placeIDSearch["result"]["formatted_address"])
+            if "website" in placeIDSearch["result"]: print(placeIDSearch["result"]["website"])
+            print("==============================")
 
             time.sleep(2)
-            
 
-            if verbose == True:
-                
-                if "name" in placeSearch["result"]: print(placeSearch["result"]["name"])
-                else: print("No Name Available")
-
-                if "formatted_phone_number" in placeSearch["result"]: print(placeSearch["result"]["formatted_phone_number"])
-                else: print("No Number Available")
-
-                if "formatted_address" in placeSearch["result"]: print(placeSearch["result"]["formatted_address"])
-                else: print("No Address Available")
-
-                if "website" in placeSearch["result"]: print(placeSearch["result"]["website"])
-                else: print("No Website Available")
-
-                print("===================================")
-            
-            if "name" in placeSearch["result"]:nameList.append(placeSearch["result"]["name"])
-            else: nameList.append("N/a")
-            
-
-            if "formatted_phone_number" in placeSearch["result"]: numberList.append(placeSearch["result"]["formatted_phone_number"])
-            else: numberList.append("N/a")
-
-            if "formatted_address" in placeSearch["result"]: addressList.append(placeSearch["result"]["formatted_address"])
-            else: addressList.append("N/a")
-
-            if "website" in placeSearch["result"]: websiteList.append(placeSearch["result"]["website"])
-            else: websiteList.append("N/a")
-
+    if(mode == "csv" or mode == "both"):
+        
+        for x in placeIDList:
+            placeIDSearch = client.place(x)
+            if "name" in placeIDSearch["result"]: nameList.append(placeIDSearch["result"]["name"])
+            else:nameList.append("N/a")
+            if "formatted_phone_number" in placeIDSearch["result"]: numberList.append(placeIDSearch["result"]["formatted_phone_number"])
+            else:numberList.append("N/a")
+            if "formatted_address" in placeIDSearch["result"]: addressList.append(placeIDSearch["result"]["formatted_address"])
+            else:addressList.append("N/a")
+            if "website" in placeIDSearch["result"]: websiteList.append(placeIDSearch["result"]["website"])
+            else:websiteList.append("N/a")
         df = pd.DataFrame({"name":[x for x in nameList],
                            "number":[x for x in numberList],
                            "address":[x for x in addressList],
                            "website":[x for x in websiteList]
                            })
-        
-        df.to_csv(f"{fileName}.csv", index=False)
+        df.to_csv(f"{filename}.csv", index=False)
 
 
-    def printPlaces(self, listofids):
 
-        key = None
+    #if mode == print, print all results
+    #if mode == csv, paste all results into CSV file
+    #if mode == both, print all results and pastes into csv file
+    
 
-        #with open("key.txt", "r") as apiKey: key = apiKey.readline()
 
-        client = googlemaps.Client(key=self.key)     
-        count = 0
-        for x in listofids:
-            count = count + 1
-            placeSearch = client.place(x)
-            time.sleep(1)
-            print(f"{count} -=================================")
 
-            if "name" in placeSearch["result"]: print(placeSearch["result"]["name"])
-            else: print("No Name Available")
 
-            if "formatted_phone_number" in placeSearch["result"]: print(placeSearch["result"]["formatted_phone_number"])
-            else: print("No Number Available")
-
-            if "formatted_address" in placeSearch["result"]: print(placeSearch["result"]["formatted_address"])
-            else: print("No Address Available")
-
-            if "website" in placeSearch["result"]: print(placeSearch["result"]["website"])
-            else: print("No Website Available")
 
 pythonversion = "Python 3.10.12"
 version = "1.2"
@@ -284,60 +121,71 @@ cliMenu = """1 - Configure Search
 3 - Print Results to Screen
 4 - Exit"""
 
+import maskpass
 os.system("clear")
 print(Fore.RED + "Enter Google Maps API Key (Must be 39 characters)")
-key = input(Fore.WHITE + "> ").strip()
+key = maskpass.askpass(Fore.WHITE + "> ", mask="*").strip()
 if(len(key) != 39):
     print("API Key must be 39 characters exactly")
     exit()
 fulfilled = False
 
 while True:
-    os.system("clear")
+    #os.system("clear")
     print(Fore.RED + "Welcome to SweetMapper " + version)
 
     print(Fore.WHITE + cliMenu)
     cliInput = input("> ")
+    os.system("clear")
+    
     match cliInput:
         case "1":
-            os.system("clear")
+            #os.system("clear")
             print("Enter Place Search (e.g. Restaurant)")
             placeType = input("> ")
-            os.system("clear")
-            print("Enter Location (e.g. Manchester)")
-            location = input("> ")
-            os.system("clear")
+            #os.system("clear")
+            print("Enter City (Press ENTER to enter your own Lat:Lng values)")
+            loc = str(input("> ").lower().strip())
+            if(loc == ""):
+                print("Enter Latitudinal Value (e.g. -33.8599358)")
+                lat = float(input("> "))
+                #os.system("clear")
+                print("Enter Longitudinal Value (e.g. 151.2090295)")
+                lng = float(input("> "))
+            #os.system("clear")
             print("Enter Radius (e.g. 20)")
             radius = input("> ")
-            os.system("clear")
+            #os.system("clear")
             print("Enter Country (e.g. UK)")
             country = input("> ")
-            os.system("clear")
+            #os.system("clear")
             print("Enter Language (e.g. en-UK)")
             language = input("> ")
-            os.system("clear")
-            print("Open Now? (e.g. True)")
-            open_now = bool(input("> "))
             fulfilled = True
         case "2":
             os.system("clear")
             if(fulfilled):
                 print("File Name (e.g \"sweetmap\")")
                 filename = input("> ")
+                if(loc != ""):
+                    areaSearch("csv", key=key, placeType=placeType, location=loc, radius=radius, language=language, filename=filename)
+                else:
+                    areaSearch("csv", key=key, placeType=placeType, location={"lat": lat, "lng": lng}, radius=radius, language=language, filename=filename)
 
-                plsr = Places(key, f"{placeType} {location}", location, radius, country, language, open_now=open_now)
-                df = plsr.areaSearch()
-                plsr.toCSV(plsr.getPlaceIds(df), filename)
             else:
                 print(Fore.RED + "Please Fulfill Configure Search First")
                 l = input(Fore.WHITE + "Press Enter To Go Back > ")
         case "3":
             if(fulfilled):
-
-                plsr = Places(key, f"{placeType} {location}", location, radius, country, language, open_now=open_now)
-                df = plsr.areaSearch()
-                plsr.printPlaces(plsr.getPlaceIds(df))
+                if(loc != ""):
+                    areaSearch("print", key=key, placeType=placeType, location=loc, radius=radius, language=language, filename=filename)
+                else:
+                    areaSearch("print", key=key, placeType=placeType, location={"lat": lat, "lng": lng}, radius=radius, language=language, filename=filename)
                 print("===================================")
+
+            else:
+                print(Fore.RED + "Please Fulfill Configure Search First")
+                l = input(Fore.WHITE + "Press Enter To Go Back > ")
         case "4":
             break
 
